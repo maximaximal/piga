82561a939b9d436e19df46968f6e8649deb54270
Max Heisinger <mail@maximaximal.com>
2014-10-30 15:22:38 +0100

    Integrated a sigc++ signal in the game event handler class.

--- libpiga/include/piga/GameEventHandler.hpp
+++ libpiga/include/piga/GameEventHandler.hpp
@@ -1,6 +1,7 @@
 #ifndef LIBPIGA_PIGA_GAMEEVENTHANDLER_HPP_INCLUDED
 #define LIBPIGA_PIGA_GAMEEVENTHANDLER_HPP_INCLUDED
 
+#include <sigc++/signal.h>
 #include <piga/GameEvent.hpp>
 
 namespace piga
@@ -11,10 +12,18 @@ namespace piga
     class GameEventHandler
     {
         public:
+            typedef sigc::signal<void, const GameEvent&, float> GameEventSignal;
+
             void sendGameEvent(const GameEvent &gameEvent, float frametime);
 
+            /**
+             * @brief Gets the game event signal. The signal is fired when an event occurs.
+             * @return A reference to the game event signal of this handler.
+             */
+            GameEventSignal& getGameEventSignal();
         protected:
             virtual void onGameEvent(const GameEvent &gameEvent, float frametime);
+            GameEventSignal m_signal;
     };
 }
 
--- libpiga/src/GameEventHandler.cpp
+++ libpiga/src/GameEventHandler.cpp
@@ -5,6 +5,11 @@ namespace piga
     void GameEventHandler::sendGameEvent(const GameEvent &gameEvent, float frametime)
     {
         onGameEvent(gameEvent, frametime);
+        m_signal(gameEvent, frametime);
+    }
+    GameEventHandler::GameEventSignal &GameEventHandler::getGameEventSignal()
+    {
+        return m_signal;
     }
     void GameEventHandler::onGameEvent(const GameEvent &gameEvent, float frametime)
     {
