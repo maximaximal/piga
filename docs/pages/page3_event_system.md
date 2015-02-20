3363b217e2fe2f63d736c4abc28a41af3e02142a
Max Heisinger <mail@maximaximal.com>
2015-02-20 23:39:13 +0100

    Wrote documentation for the GameInput class + a event system page.

---
+++ docs/pages/page3_event_system.md
@@ -0,0 +1,44 @@
+Event System {#event_system}
+-----------
+
+The event system is divided into several different modules:
+
+  1. The piga::GameEvent class.
+  2. The piga::event Namespace and contained classes. (piga::event::GameInput, piga::event::TextInput)
+  3. The piga::GameControl enumeration which provides the type of events. 
+  4. The piga::Interface and piga::Host classes for event dispatching 
+     between apps/games and the host application.
+     
+### State vs Value
+
+The piga::event::GameInput class has 2 variants of describing inputs: 
+
+  1. A boolean state value. 
+  2. A integer value. 
+
+#### State (bool)
+
+The state abstraction serves the purpose of an easier method to hande inputs. Some games
+only need boolean inputs and can use this function without heavier comparison operators. 
+
+In addition, the state abstraction is easier for pigaco developers. Keyboard inputs or digital 
+buttons only need a true and a false value which is comppletely handled by a boolean. The 
+piga::event::GameInput class maps the state directly to the integer value with only a minimal
+performance drop, which means that this abstraction does not hurt anybody. 
+
+#### Value (int)
+
+The integer value is the internal representation of the input value in the piga::event::GameInput class. 
+This is an integer to provide the possibility of floating point inputs (analog joysticks or similar devices) without 
+the introduction of another event type. 
+
+#### Translation
+
+States can be translated into values (and values into states) using this conversion
+table:
+
+State | Value
+----- |  -----
+true  | 100000
+false | 0
+
--- libpiga/include/piga/Events/GameInput.hpp
+++ libpiga/include/piga/Events/GameInput.hpp
@@ -7,24 +7,123 @@ namespace piga
 {
     namespace event
     {
+        /**
+         * @brief A structure for handling game events like buttons and joysticks. 
+         * 
+         * This structure tries to simplify the handling of game events. Game events are explained 
+         * on the @ref event_system "event system page".
+         */
         class GameInput
         {
             public:
+                /**
+                 * @brief Constructs the GameInput instance. 
+                 * 
+                 */
                 GameInput();
+                /**
+                 * @brief Construct the GameInput instance with a defined GameControl and a state. (true/false inputs)
+                 * 
+                 * Value of the internal int value:
+                 * 
+                 * <table>
+                 * <tr>
+                 * <th>State</th>
+                 * <th>Value</th>
+                 * </tr>
+                 * <tr>
+                 * <td>true</td>
+                 * <td>100000</td>
+                 * </tr>
+                 * <tr>
+                 * <td>false</td>
+                 * <td>0</td>
+                 * </tr>
+                 * </table>
+                 * 
+                 * @param control The GameControl-value of the input. 
+                 * @param state The state of the input. True means, that the button has been pressed, false means the opposite. 
+                 */
                 GameInput(GameControl control, bool state);
+                /**
+                 * @brief Construct the GameInput instance with a defined GameControl and a value (analog inputs).
+                 * 
+                 * @param control The GameControl-value of the input.
+                 * @param value The value of the input. 0 equals the state false, 100000 equals the state true.
+                 */
                 GameInput(GameControl control, int value);
+                /**
+                 * @brief Destructor of the GameInput instance. 
+                 */
                 virtual ~GameInput();
 
+                /**
+                 * @brief Returns the state of the GameControl of the player. 
+                 * 
+                 * This interprets the internal int value of the GameControl. Everything except 0
+                 * will be returned as true.  
+                 * 
+                 * @return The state of the GameControl. 
+                 */
                 bool state() const;
+                /**
+                 * @brief Returns the value of the GameControl of the player. 
+                 * 
+                 * @return The raw value of the GameControl. 
+                 */
                 int value() const;
+                /**
+                 * @brief Returns the control of the GameInput event. 
+                 * 
+                 * @return The control of this GameInput event. 
+                 */
                 GameControl control() const;
 
+                /**
+                 * @brief Sets the internal int value of the event to the desired state. 
+                 * 
+                 * Value of the internal int value:
+                 * 
+                 * <table>
+                 * <tr>
+                 * <th>State</th>
+                 * <th>Value</th>
+                 * </tr>
+                 * <tr>
+                 * <td>true</td>
+                 * <td>100000</td>
+                 * </tr>
+                 * <tr>
+                 * <td>false</td>
+                 * <td>0</td>
+                 * </tr>
+                 * </table>
+                 * 
+                 * @param state ...
+                 * @return void
+                 */
                 void setState(bool state);
+                /**
+                 * @brief Sets the internal value directly. 
+                 * 
+                 * @param value The value of the GameInput event to be set. 
+                 */
                 void setValue(int value);
+                /**
+                 * @brief Sets the control of the GameEvent. 
+                 * 
+                 * @param control The GameControl of the GameEvent.
+                 * @return void
+                 */
                 void setControl(GameControl control);
 
                 bool operator==(const GameControl &rightControl) const;
                 GameInput& operator=(const GameInput &otherInput);
+                /**
+                 * @brief Shorter function in operator form for the GameInput::state() method. 
+                 * 
+                 * @return True if the GameInput::state() function returned true, false otherwise. 
+                 */
                 operator bool() const;
             private:
                 GameControl m_control;
