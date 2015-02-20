7c79d6d805ffe56ab8db5da0c2e2deb3a8280db7
Max Heisinger <mail@maximaximal.com>
2015-02-20 22:53:47 +0100

    Wrote documentation for the piga::GameEvent class.

--- libpiga/include/piga/GameEvent.hpp
+++ libpiga/include/piga/GameEvent.hpp
@@ -16,6 +16,11 @@ namespace piga
     class GameEvent
     {
         public:
+            /**
+             * @brief Type of the event. 
+             * 
+             * This enum defines the existing event types. 
+             */
             enum GameEventType {
                 GameInput,
                 TextInput,
@@ -25,16 +30,59 @@ namespace piga
             };
 
 
+            /**
+             * @brief Constructs a GameEvent using another GameEvent instance. 
+             * 
+             * @param gameEvent The other event to copy from.
+             */
             GameEvent(const GameEvent &gameEvent);
+            /**
+             * @brief Constructs a GameEvent with only the player id as a parameter.
+             * 
+             * @param playerID The player id of the player who was the origin of the event.
+             */
             GameEvent(int playerID = 0);
+            /**
+             * @brief Constructs a GameEvent using a GameInput instance.
+             * 
+             * @param playerID The player id of the player who was the origin of the event.
+             * @param gameInput The GameInput instance to copy from. 
+             */
             GameEvent(int playerID, const event::GameInput &gameInput);
+            /**
+             * @brief Constructs a GameEvent using a TextInput instance.  
+             * 
+             * @param playerID The player id of the player who was the origin of the event.
+             * @param textInput The TextInput instance to copy from.
+             */
             GameEvent(int playerID, const event::TextInput &textInput);
+            /**
+             * @brief Destructs the GameEvent instance. 
+             */
             virtual ~GameEvent();
 
+            /**
+             * @brief Sets the player id of this GameEvent instance. 
+             * 
+             * @param playerID The player id to be set as an origin of this GameEvent. 
+             */
             void setPlayerID(int playerID);
+            /**
+             * @brief Returns the id of the player who created this GameEvent.
+             * 
+             * @return The ID of the player.
+             */
             int playerID() const;
+            /**
+             * @brief Returns the GameEventType of this event. 
+             * 
+             * @return The GameEventType of this GameEvent.
+             */
             GameEventType type() const;
 
+            /**
+             * @brief Holds the event classes. 
+             */
             union
             {
                 event::GameInput gameInput;
@@ -42,7 +90,6 @@ namespace piga
             };
 
             GameEvent& operator=(const GameEvent &otherEvent);
-
             void operator=(const event::GameInput &gameInput);
             void operator=(const event::TextInput &textInput);
         private:
