f37e4936966a12fcafc3d91edacc9e1c17c517b4
Max Heisinger <mail@maximaximal.com>
2015-03-30 02:55:25 +0200

    Wrote documentation for the player manager.

--- libpiga/include/piga/PlayerManager.hpp
+++ libpiga/include/piga/PlayerManager.hpp
@@ -8,16 +8,49 @@
 
 namespace piga
 {
+    /**
+     * @brief The PlayerManager class manages access to the players on the current piga console.
+     *
+     * Players in the manager may be saved in shared memory (if the piga::Interface is not selfhosted) or
+     * locally in the manager.
+     */
     class PlayerManager
     {
         public:
+            /**
+             * @brief Construct the player manager.
+             * @param sharedMemory Defines if shared memory should be used. If true, the manager
+             * will try to access the players from shared memory. If false, the
+             * manager will create the players itself.
+             *
+             * @param playerNum The maximum number of players.
+             */
             PlayerManager(bool sharedMemory = true, int playerNum = 16);
+            /**
+             * @brief Destructor of the player manager.
+             */
             virtual ~PlayerManager();
 
+            /**
+             * @brief Initiates the player manager.
+             *
+             * When this function gets called, the manager tries to access the shared memory
+             * or to create the internal player array. The host-application (pigaco) has to
+             * be running before this function is called, or else shared memory will not work!
+             */
             void init();
 
+            /**
+             * @brief Returns the specified player from the internal/shared player array.
+             * @param playerID The ID of the player.
+             * @return A raw pointer to the player. Do not delete this pointer, the player is owned by the manager!
+             */
             Player *getPlayer(int playerID);
 
+            /**
+             * @brief Returns the maximum number of players.
+             * @return The maximum number of players.
+             */
             int size();
         private:
             boost::interprocess::managed_shared_memory *m_players = nullptr;
